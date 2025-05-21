f9d1b7fa1e28192071b2f41f3c27a47291f49f09
ADITdeveloper <adit.gam775@gmail.com>
2025-05-21 20:04:30 +0700

    Delete ai_engine.py

--- ai_engine.py
+++
@@ -1,6 +0,0 @@
-
-from transformers import pipeline
-qa = pipeline('text-generation', model='gpt2')
-def ask(prompt):
-    result = qa(prompt, max_length=100, num_return_sequences=1)
-    return result[0]['generated_text']
