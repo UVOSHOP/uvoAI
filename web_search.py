c37e9614f39137450304d8075ef970d86753cca2
ADITdeveloper <adit.gam775@gmail.com>
2025-05-21 20:04:52 +0700

    Delete web_search.py

--- web_search.py
+++
@@ -1,7 +0,0 @@
-
-from duckduckgo_search import ddg
-def web_answer(query):
-    results = ddg(query, max_results=1)
-    if results:
-        return results[0]['body']
-    return "Maaf, aku tidak menemukan informasi yang kamu cari."
